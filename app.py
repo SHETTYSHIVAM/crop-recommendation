cab5e83c0bb192d5b5574941b0b4951f2d210036
SHIVAM SHETTY <shettyshivam209@gmail.com>
2024-11-01 18:41:06 +0530

    added fertilizer option

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import requests
 import pprint
 from constants import API_KEY
+import pandas as pd
 app = Flask(__name__)
 
 # # Load your model
@@ -19,11 +20,23 @@ model_paths = {
     "RandomForestClassifier" : "models/RandomForestClassifier.pkl",
 }
 
-models = dict()
+crop_predictor = pickle.load(open('models/RandomForestClassifier.pkl', 'rb'))
 
-for model_name, model_path in model_paths.items():
-    models[model_name] = pickle.load(open(model_path, 'rb'))
-    
+# for model_name, model_path in model_paths.items():
+#     models[model_name] = pickle.load(open(model_path, 'rb'))
+
+fertilizer_predictor = pickle.load(open('models/fertilizer_recommendation.pkl', 'rb'))
+
+
+def get_weather_info(latitude, longitude):
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}"
+    response = requests.get(url)
+    data = eval(response.content)
+    weather = data['main']
+    pprint.pprint(weather)
+    temp = weather['temp']
+    humidity = weather['humidity']
+    return temp, humidity
 
 @app.route('/', methods=['GET', 'POST'])
 def get_crop():
@@ -35,8 +48,6 @@ def get_crop():
             P = int(data.get('P', 0))
             K = int(data.get('K', 0))
             pH = float(data.get('pH',7))
-            selected_model_str = data.get('selectedModel', 'DecisionTreeClassifier')
-            selected_model = models[selected_model_str]
             latitude = float(data.get('latitude', 0.0))
             longitude = float(data.get('longitude', 0.0))
 
@@ -52,12 +63,12 @@ def get_crop():
             
             test_input = np.array([N, P, K, temp-273.15, humidity, pH, 200.098026]).reshape(1, 7)
             
-            result = selected_model.predict(test_input)
+            result = crop_predictor.predict(test_input)
 
             X_test = np.load('X_test.npy', allow_pickle=True)
             y_test = np.load('y_test.npy', allow_pickle=True)
 
-            accuracy = selected_model.score(X_test, y_test)
+            accuracy = crop_predictor.score(X_test, y_test)
             
             crop = result[0]
             print(f"Predicted Crop: {crop}")
@@ -68,7 +79,46 @@ def get_crop():
         except :
             print(f"Error during prediction:")
             return jsonify({"success":False, "message": "Server Error, Try again later"}), 500  # Return 500 on error
-    return render_template('index.html', crop=crop, models = list(models.keys()))
+    return render_template('crop.html', crop=crop)
+
+
+@app.route('/fertilizer', methods=['GET', 'POST'])
+def get_fertilizer():
+    if request.method == 'POST':
+        # Moisture	Nitrogen	Potassium	Phosphorous Soil Type	Crop Type
+        data = request.get_json()
+        moisture = int(data.get('moisture', 0))
+        N = int(data.get('nitrogen', 0))
+        P = int(data.get('phosphorous', 0))
+        K = int(data.get('potassium', 0))
+        soil_type = (data.get('soilType', 'Clayey'))
+        crop_type = (data.get('cropType', 'Paddy'))
+        latitude = float(data.get('latitude', 0.0))
+        longitude = float(data.get('longitude', 0.0))
+        # temp = float(data.get('temp'))
+        # humidity = float(data.get('humidity'))
+        temp, humidity = get_weather_info(latitude, longitude)
+
+        pprint.pprint(data)
+
+    #     test_input = np.array([temp-273.15, humidity, moisture,  N, K, P, soil_type, crop_type]).reshape(1, 8)
+    #     test_input = pd.DataFrame(np.array([[89, 65, 32, 24, 23, 34, 'Clayey', 'Millets']]), 
+    #                            columns=['Temparature', 'Humidity ', 'Moisture', 'Nitrogen', 'Potassium',
+    #    'Phosphorous', 'Soil Type', 'Crop Type'])
+    #     result = fertilizer_predictor.predict(test_input)
+    #     fertilizer = result[0]
+
+        test_input = pd.DataFrame([[temp-273.15, humidity, moisture, N, K, P, soil_type, crop_type]],
+                                  columns=['Temparature', 'Humidity ', 'Moisture', 'Nitrogen', 'Potassium','Phosphorous', 'Soil Type', 'Crop Type'])
+
+        result = fertilizer_predictor.predict(test_input)
+        fertilizer = result[0]
+
+        print('Recommended Fertilizer: ', fertilizer)
+        return jsonify({'success':True, 'fertilizer': fertilizer})
+    else:
+        return render_template('fertilizer.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
